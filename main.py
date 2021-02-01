81f3b5c24cad88c01294379429900870558bfbef
Daisy Butler <daisybutler16@gmail.com>
2021-02-01 10:42:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from scraper import JobScrape
+
+mon = JobScrape("monster")
+
+print("Working...")
+
+mon_results = mon.get_jobs("dublin", "ie", "python,developer")
+
+print(mon_results)
+
+for result in mon_results:
+    print(f"Job Title: {result['title']}")
+    print(f"Company: {result['company']}")
+    print(f"URL: {result['url']}")
+    if "description" in result:
+        print(f"Description: {result['description']}")
+
+print(f"{len(mon_results)} jobs found")
